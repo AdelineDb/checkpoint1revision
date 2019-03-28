7ace3c94e17af9ce1cb996fbb05f7cd397e13228
AdelineDb <adeline.dubosc@hotmail.fr>
2019-03-28 16:06:54 +0100

    modif style

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-Dico du geek /// mot du jour : Poper: paouwfffff
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Pascal_Project</title>
+</head>
+
+<body>
+
+<h1>Dico du geek</h1>
+
+<p> Mot du jour 1:
+    <strong>Poper</strong>: paouwfffff</p>
+
+
+</body>
+</html>
